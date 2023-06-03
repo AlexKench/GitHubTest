6811520ae868b95b4327fdff06036334f50f60fb
Alexander <sasha.maks92@gmail.com>
2023-06-03 13:46:16 +0300

    Test2 тест

---
+++ test2/Test2.java
@@ -0,0 +1,14 @@
+package test2;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+
+        test2();
+
+    }
+
+    public static void test2(){
+        System.out.println("Hello main page");
+    }
+}
