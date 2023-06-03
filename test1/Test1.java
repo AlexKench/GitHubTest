4261ffbbbd7f7eadc3d3e81fef95929dd22155e6
Alexander <sasha.maks92@gmail.com>
2023-06-03 13:09:55 +0300

    Test1

---
+++ test1/Test1.java
@@ -0,0 +1,15 @@
+package test1;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(test1(2, 2));
+
+    }
+
+
+    public static int test1(int a, int b){
+        return a + b;
+    }
+}
